1e9795c95332cdf219dc56986633d7680b98f052
Wkatir <110741874+wkatir@users.noreply.github.com.>
2025-01-08 10:15:24 -0600

    feat: configure Cloudinary and set up folder paths

--- main.py
+++ main.py
@@ -5,3 +5,20 @@ import os
 import requests
 from dotenv import load_dotenv
 
+# Cargar variables del archivo .env
+load_dotenv()
+
+# Configurar credenciales de Cloudinary desde .env
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
+)
+
+# Obtener el directorio actual
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Definir las carpetas
+input_folder = os.path.join(current_dir, "imagenes")  # Carpeta de imágenes originales
+output_folder = os.path.join(current_dir, "imagenes_procesadas")  # Carpeta de imágenes procesadas
+
